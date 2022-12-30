82d7dc8b0c2b65c880d1c466030ebb7092a14b22
heythisisaj <85194859+heythisisaj@users.noreply.github.com>
2022-12-30 17:24:34 -0500

    Added SQL Express db and updated the DB

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Vidly.Controllers;
 
 namespace Vidly.Models
 {
@@ -19,7 +20,12 @@ namespace Vidly.Models
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+
+
     {
+        public DbSet <Customer> Customers { get; set; }
+        public DbSet <Movie> Movies { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
